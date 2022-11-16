62e376b4d13109e0524ce2e17aa56b069635ad75
bingba2022 <bingbakim@gmail.com>
2022-11-16 11:52:32 +0900

    added reviews

---
+++ reviews/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from common.models import CommonModel
+
+# Create your models here.
+
+
+class Review(CommonModel):
+
+    """Review from a User to a Room or Experience"""
+
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+
+    room = models.ForeignKey(
+        "rooms.Room",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+
+    experience = models.ForeignKey(
+        "experiences.Experience",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+    # Even the user is deleted, maybe the reviews might still stays on a room or an experience
+
+    payload = models.TextField()
+    rating = models.PositiveBigIntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.user} / {self.rating}⭐"
