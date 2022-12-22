226397b0a2d23288b17e9932c9532d63240df375
bingba2022 <bingbakim@gmail.com>
2022-12-22 21:53:18 +0900

    Using serializer

--- categories/serializers.py
+++ categories/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class CategorySerializer(serializers.Serializer):
     # field that we want to expose about our categories
     # explaining to the serializer how should the name and kind should be represented when we turn then into json
-    pk = serializers.IntegerField()
+    pk = serializers.IntegerField(read_only=True)
     name = serializers.CharField(
         required=True,
         max_length=50,
@@ -12,4 +12,4 @@ class CategorySerializer(serializers.Serializer):
     kind = serializers.CharField(
         max_length=15,
     )
-    created_at = serializers.DateTimeField()
+    created_at = serializers.DateTimeField(read_only=True)
