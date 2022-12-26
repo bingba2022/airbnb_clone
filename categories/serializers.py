1d9b64e28073fcb7121f06015917d8bbc2f9b6a9
bingba2022 <bingbakim@gmail.com>
2022-12-27 02:24:16 +0900

    added create and update method

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from .models import Category
 
 
 class CategorySerializer(serializers.Serializer):
@@ -9,7 +10,22 @@ class CategorySerializer(serializers.Serializer):
         required=True,
         max_length=50,
     )
-    kind = serializers.CharField(
-        max_length=15,
+    kind = serializers.ChoiceField(
+        choices=Category.CategoryKindChoices.choices,
     )
     created_at = serializers.DateTimeField(read_only=True)
+
+    # create method has to return an object
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+        # ** will take the dictionary and furn it into this
+        # name = validated_data['name'] automatically
+
+    def update(
+        self, instance, validated_data
+    ):  # instance: category (database data) # validated_data: data from user
+        # updating with the data that user has given but if the user didn't send the data then leave it as it was (instance.name/instance.kind)
+        instance.name = validated_data.get("name", instance.name)
+        instance.kind = validated_data.get("kind", instance.kind)
+        instance.save()
+        return instance
--- categories/views.py
+++ categories/views.py
@@ -3,6 +3,7 @@ from .models import Category
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import CategorySerializer
+from rest_framework.exceptions import NotFound
 
 # from django.core import serializers
 # from django.http import JsonResponse
@@ -21,13 +22,35 @@ def categories(request):
     elif request.method == "POST":
         # giving data that user sent to serializer
         serializer = CategorySerializer(data=request.data)
-        print(serializer.is_valid())
-        print(serializer.errors)
-        return Response({"created": True})
+        if serializer.is_valid():
+            new_category = serializer.save()
+            CategorySerializer(new_category).data
+            return Response({"created": True})
+        else:
+            return Response(serializer.errors)
 
 
-@api_view()
+@api_view(["GET", "PUT"])
 def category(request, pk):
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+    try:  # 1. get the category
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound  # raise 밑에 있는 코드를 다 죽임 (실행 x)
+
+    if request.method == "GET":  # if the user wants GET
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    elif request.method == "PUT":  # if the user wants PUT
+        category = Category.objects.get(pk=pk)
+        # create a serializer that has a category from database + the data that the user sent
+        serializer = CategorySerializer(
+            category,
+            data=request.data,
+            partial=True,  # telling that we are just editing the data not creating
+        )
+
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
