1cc1dfc8cedf599ba32bc86caf6b08b2eba1821b
bingba2022 <bingbakim@gmail.com>
2022-12-30 19:26:09 +0900

    changed to ModelViewSet

--- categories/urls.py
+++ categories/urls.py
@@ -2,9 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Categories.as_view()),  # if we put a class name here we have to put as_view()
+    path(
+        "",
+        views.CategoryViewSet.as_view(
+            {
+                "get": "list",
+                "post": "create",
+            }
+        ),
+    ),  # if we put a class name here we have to put as_view()
     path(
         "<int:pk>",
-        views.CategoryDetail.as_view(),
+        views.CategoryViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "partial_update",
+                "delete": "destroy",
+            }
+        ),
     ),
 ]
--- categories/views.py
+++ categories/views.py
@@ -6,54 +6,9 @@ from .serializers import CategorySerializer
 from rest_framework.exceptions import NotFound
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.views import APIView
-
-# from django.core import serializers
-# from django.http import JsonResponse
+from rest_framework.viewsets import ModelViewSet
 
 
-# Create your views here.
-
-
-class Categories(APIView):
-    def get(self, request):
-        all_categories = Category.objects.all()
-        serializer = CategorySerializer(
-            all_categories,
-            many=True,
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            new_category = serializer.save()
-            return Response(
-                CategorySerializer(new_category).data,
-            )
-        else:
-            return Response(serializer.errors)
-
-
-class CategoryDetail(APIView):
-    def get_object(self, pk):
-        try:  # 1. get the category
-            return Category.objects.get(pk=pk)
-        except Category.DoesNotExist:
-            raise NotFound  # raise 밑에 있는 코드를 다 죽임 (실행 x)
-
-    def get(self, request, pk):
-        serializer = CategorySerializer(self.get_object(pk))
-        print(serializer)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        serializer = CategorySerializer(self.get_object(pk), data=request.data, partial=True)
-        if serializer.is_valid():
-            updated_category = serializer.save()
-            return Response(CategorySerializer(updated_category).data)
-        else:
-            return Response(serializer.errors)
-
-    def delete(self, request, pk):
-        self.get_object(pk).delete()
-        return Response(status=HTTP_204_NO_CONTENT)
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
