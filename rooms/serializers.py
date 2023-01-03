9909010b02b461038018d193d5e5078629dfe789
bingba2022 <bingbakim@gmail.com>
2023-01-03 16:23:34 +0900

    added amenities, categories and owner to the room

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -27,11 +27,19 @@ class AmenitySerializer(ModelSerializer):
 
 class RoomDetailSerializer(ModelSerializer):
 
-    owner = TinyUserSerializer()
-    amenities = AmenitySerializer(many=True)  # only use many=True if it is an array
-    category = CategorySerializer()  # Room model 이랑 이름 맞는지 확인
+    owner = TinyUserSerializer(read_only=True)
+    amenities = AmenitySerializer(
+        read_only=True,
+        many=True,
+    )  # only use many=True if it is an array
+    category = CategorySerializer(
+        read_only=True,
+    )  # Room model 이랑 이름 맞는지 확인
 
     class Meta:
         model = Room
         fields = "__all__"
         depth = 1
+
+    def create(self, validated_data):
+        return Room.objects.create(**validated_data)
--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError
 from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Amenity, Room
 from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
-
+from categories.models import Category
 
 # api/v1/rooms/amenities
 class Amenities(APIView):
@@ -59,6 +59,41 @@ class Rooms(APIView):
         all_rooms = Room.objects.all()
         serializer = RoomListSerializer(all_rooms, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        # user that requested this url
+        if request.user.is_authenticated:
+            serializer = RoomDetailSerializer(data=request.data)
+            if serializer.is_valid():
+                category_pk = request.data.get("category")
+                if not category_pk:
+                    # category가 입력되지 않았을때
+                    raise ParseError("Category is required.")
+                try:
+                    category = Category.objects.get(pk=category_pk)
+                    if category.kind == Category.CategoryKindChoices.EXPERIENCES:
+                        # Category 가 experiences 일때
+                        raise ParseError("The category kind should be 'rooms'")
+                except Category.DoesNotExist:  # 없는 Category가 입력되었을때
+                    raise ParseError("Category not found")
+                room = serializer.save(
+                    owner=request.user,
+                    category=category,
+                )
+                amenities = request.data.get("amenities")
+                for amenity_pk in amenities:
+                    try:
+                        amenity = Amenity.objects.get(pk=amenity_pk)
+                    except Amenity.DoesNotExist:
+                        raise ParseError(f"Amenities Does Not Exist{amenity_pk}")
+                        room.delete()
+                    room.amenities.add(amenity)
+                serializer = RoomDetailSerializer(room)
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors)
+        else:
+            raise NotAuthenticated
 
 
 class RoomDetail(APIView):
